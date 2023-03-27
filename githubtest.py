379a35216c55abd31b4160ba608d1eefc809c96e
CSgitrepo <129018435+CSgitrepo@users.noreply.github.com>
2023-03-26 21:35:55 -0400

    Add files via upload

---
+++ githubtest.py
@@ -0,0 +1,10 @@
+
+def returnFunction():
+    return "Although flesh may blacken and fail, fear not, for this too can be replaced. You can be reincarnated - reborn in steel by the will of the Omnissiah."
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    var = returnFunction()
+    print(var)
+
+
